303583bd8491a464951ec2fdbb322c92d6c51f91
tgourdel <github@t.gourdel.fr>
2019-12-15 12:20:50 -0800

    license path

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo "Hello Talend user"
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="${INPUT_LICENSE_PASSPHRASE}" \
---output $HOME/secrets/license $GITHUB_WORKSPACE/$LICENSE_PATH
+--output $HOME/secrets/license "${INPUT_LICENSE_PATH}"
 
 echo "You are going to build ${INPUT_PROJECT} project"
 echo "Maven Command: mvn -s /maven-settings.xml -f ${GITHUB_WORKSPACE}/${INPUT_PROJECT}/poms/pom.xml -Pcloud-publisher clean deploy $*"
