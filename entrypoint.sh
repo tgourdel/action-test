97e75b93e9794453dc04959c5df3026406c65b2b
tgourdel <github@t.gourdel.fr>
2019-12-15 10:46:44 -0800

    Remove some options

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ echo "Hello Talend user"
 
 export MAVEN_OPTS="-Dlicense.path=n/a -Dupdatesite.path=${INPUT_P2_URL} -Dservice.url=${INPUT_CLOUD_URL} -Dcloud.token=${INPUT_CLOUD_TOKEN} -Dcloud.publisher.screenshot=${INPUT_SCREENSHOT}"
 
-mvn -s /maven-settings.xml -f ${GITHUB_WORKSPACE}/${INPUT_PROJECT_NAME}/poms/pom.xml -Pcloud-publisher clean deploy -e -pl jobs/process/${INPUT_JOB_NAME}_${INPUT_JOB_VERSION} -am
+sh -c "mvn -s /maven-settings.xml \
+              -f ${GITHUB_WORKSPACE}/${INPUT_PROJECT_NAME}/poms/pom.xml \
+               \
+             -Pcloud-publisher clean deploy $*"
 
 
+#    -e  -am -pl jobs/process/${INPUT_JOB_NAME}_${INPUT_JOB_VERSION}
